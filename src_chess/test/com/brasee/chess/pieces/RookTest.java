76084257e0391a98685af8d3b60ee64d574a1c5c
kbrasee <kbrasee@63b2f32a-31a6-11de-94d6-134fa40061eb>
2009-04-26 17:17:26 +0000

    Added Rook

---
+++ src_chess/test/com/brasee/chess/pieces/RookTest.java
@@ -0,0 +1,89 @@
+package com.brasee.chess.pieces;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.brasee.chess.Square;
+
+public class RookTest {
+
+	private Piece rook;
+	
+	@Before
+	public void setUp() {
+		rook = new Rook(Piece.WHITE);
+	}
+	
+	@Test
+	public void testRookCanMoveLeft() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("a5");
+		assertTrue(rook.canMove(currentSquare, emptySquare));	
+	}
+	
+	@Test 
+	public void testRookCanMoveRight() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("h5");
+		assertTrue(rook.canMove(currentSquare, emptySquare));			
+	}
+	
+	@Test
+	public void testRookCanMoveUp() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("d8");
+		assertTrue(rook.canMove(currentSquare, emptySquare));	
+	}
+	
+	@Test
+	public void testRookCanMoveDown() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("d1");
+		assertTrue(rook.canMove(currentSquare, emptySquare));	
+	}
+	
+	@Test
+	public void testRookCannotMoveDiagonal() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("e6");
+		assertFalse(rook.canMove(currentSquare, emptySquare));	
+	}
+	
+	@Test
+	public void testRookCanAttackLeft() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("a5");
+		assertTrue(rook.canAttack(currentSquare, emptySquare));	
+	}
+	
+	@Test 
+	public void testRookCanAttackRight() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("h5");
+		assertTrue(rook.canAttack(currentSquare, emptySquare));			
+	}
+	
+	@Test
+	public void testRookCanAttackUp() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("d8");
+		assertTrue(rook.canAttack(currentSquare, emptySquare));	
+	}
+	
+	@Test
+	public void testRookCanAttackDown() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("d1");
+		assertTrue(rook.canAttack(currentSquare, emptySquare));	
+	}
+	
+	@Test
+	public void testRookCannotAttackDiagonal() {
+		Square currentSquare = new Square("d5");
+		Square emptySquare = new Square("e6");
+		assertFalse(rook.canAttack(currentSquare, emptySquare));	
+	}
+	
+	
+}
