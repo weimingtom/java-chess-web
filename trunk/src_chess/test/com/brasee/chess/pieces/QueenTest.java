5c74abd8021a9c85628bb7f5a23efbf987c89113
kbrasee <kbrasee@63b2f32a-31a6-11de-94d6-134fa40061eb>
2009-05-02 03:11:04 +0000

    Updated Queen and tests to examine board in canMove and canAttack

--- trunk/src_chess/test/com/brasee/chess/pieces/QueenTest.java
+++ trunk/src_chess/test/com/brasee/chess/pieces/QueenTest.java
@@ -3,6 +3,7 @@ package com.brasee.chess.pieces;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.brasee.chess.Board;
 import com.brasee.chess.Square;
 
 import static org.junit.Assert.*;
@@ -10,149 +11,208 @@ import static org.junit.Assert.*;
 public class QueenTest {
 
 	private Piece queen;
+	private Board board;
 	
 	@Before
 	public void setUp() {
-		queen = new Queen(Piece.Color.WHITE); 
+		queen = new Queen(Piece.Color.WHITE);
+		board = new Board();
 	}
 	
 	@Test
 	public void testCanMoveStraightUp() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("d8");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveStraightDown() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("d1");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveStraightLeft() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("a5");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveStraightRight() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("h5");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveDiagonallyUpAndLeft() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("b7");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveDiagonallyUpAndRight() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("f7");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveDiagonallyDownAndLeft() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("b3");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanMoveDiagonallyDownAndRight() {
 		Square currentSquare = new Square("d5");
 		Square emptySquare = new Square("f3");
-		assertTrue(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertTrue(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCanAttackStraightUp() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("d8");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackStraightDown() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("d1");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackStraightLeft() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("a5");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackStraightRight() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("h5");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackDiagonallyUpAndLeft() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("b7");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackDiagonallyUpAndRight() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("f7");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackDiagonallyDownAndLeft() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("b3");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCanAttackDiagonallyDownAndRight() {
 		Square currentSquare = new Square("d5");
 		Square occupiedSquare = new Square("f3");
-		assertTrue(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertTrue(queen.canAttack(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCannotMoveToNonStraightOrDiagonalSquare() {
 		Square currentSquare = new Square("a1");
 		Square emptySquare = new Square("b8");
-		assertFalse(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertFalse(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCannotAttackNonStraightOrDiagonalSquare() {
 		Square currentSquare = new Square("a1");
 		Square occupiedSquare = new Square("b8");
-		assertFalse(queen.canMove(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertFalse(queen.canMove(board, currentSquare, occupiedSquare));
 	}
 	
 	@Test
 	public void testCannotMoveToSameSquare() {
 		Square currentSquare = new Square("a1");
 		Square emptySquare = new Square("a1");
-		assertFalse(queen.canMove(null, currentSquare, emptySquare));
+		board.placePiece(currentSquare, queen);
+		assertFalse(queen.canMove(board, currentSquare, emptySquare));
 	}
 	
 	@Test
 	public void testCannotAttackSameSquare() {
 		Square currentSquare = new Square("a1");
 		Square occupiedSquare = new Square("a1");
-		assertFalse(queen.canAttack(null, currentSquare, occupiedSquare));
+		board.placePiece(currentSquare, queen);
+		assertFalse(queen.canAttack(board, currentSquare, occupiedSquare));
+	}
+	
+	@Test
+	public void testCannotMoveToOccupiedSquare() {
+		Square currentSquare = new Square("a1");
+		Square occupiedSquare = new Square("b1");
+		board.placePiece(currentSquare, queen);
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.WHITE));
+		assertFalse(queen.canMove(board, currentSquare, occupiedSquare));		
+	}
+	
+	@Test
+	public void testCannotMoveWhenBlocked() {
+		Square currentSquare = new Square("a1");
+		Square emptySquare = new Square("c1");
+		board.placePiece(currentSquare, queen);
+		board.placePiece(new Square("b1"), new Queen(Piece.Color.WHITE));
+		assertFalse(queen.canMove(board, currentSquare, emptySquare));		
+	}
+	
+	@Test
+	public void testCannotAttackWhenBlocked() {
+		Square currentSquare = new Square("a1");
+		Square occupiedSquare = new Square("c1");
+		board.placePiece(currentSquare, queen);
+		board.placePiece(new Square("b1"), new Queen(Piece.Color.WHITE));
+		board.placePiece(occupiedSquare, new Queen(Piece.Color.BLACK));
+		assertFalse(queen.canAttack(board, currentSquare, occupiedSquare));		
 	}
 }
