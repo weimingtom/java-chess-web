64d9e4d5511f49fef24ca804ff395e7b4ea6eb3a
kbrasee <kbrasee@63b2f32a-31a6-11de-94d6-134fa40061eb>
2009-05-03 02:47:52 +0000

    Added tests for game turns and moves

--- trunk/src_chess/test/com/brasee/chess/GameTurnTest.java
+++ trunk/src_chess/test/com/brasee/chess/GameTurnTest.java
@@ -14,12 +14,37 @@ public class GameTurnTest {
 	}
 	
 	@Test
-	public void testPlayersCanMakeAlternatingMoves() {
+	public void testWhitePlayerCanMoveFirst() {
+		game.initializeBoard();
+		// try to move white pawn
+		assertTrue(Game.MoveType.NORMAL.equals(game.move(new Square("a2"), new Square("a3"))));
+	}
+	
+	@Test
+	public void testBlackPlayerCannotMoveFirst() {
+		game.initializeBoard();
+		// try to move black pawn
+		assertTrue(Game.MoveType.INVALID.equals(game.move(new Square("a7"), new Square("a6"))));
+	}
+	
+	@Test
+	public void testPlayersCanMakeTwoAlternatingMoves() {
 		game.initializeBoard();
 		// move white pawn
 		game.move(new Square("a2"), new Square("a3"));
 		// try to move black pawn
-		assertTrue(Game.MoveType.NORMAL.equals(game.move(new Square("a7"), new Square("a6"))));		
+		assertTrue(Game.MoveType.NORMAL.equals(game.move(new Square("a7"), new Square("a6"))));
+	}
+	
+	@Test
+	public void testPlayersCanMakeThreeAlternatingMoves() {
+		game.initializeBoard();
+		// move white pawn
+		game.move(new Square("a2"), new Square("a3"));
+		// move black pawn
+		game.move(new Square("a7"), new Square("a6"));
+		// try to move another white pawn
+		assertTrue(Game.MoveType.NORMAL.equals(game.move(new Square("h2"), new Square("h3"))));		
 	}
 	
 	@Test
@@ -30,5 +55,5 @@ public class GameTurnTest {
 		// try to move white pawn again
 		assertTrue(Game.MoveType.INVALID.equals(game.move(new Square("a3"), new Square("a4"))));
 	}
-	
+		
 }
