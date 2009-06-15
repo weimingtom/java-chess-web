37cf4aa2f19acdac814a352852664751b670af6e
kbrasee <kbrasee@63b2f32a-31a6-11de-94d6-134fa40061eb>
2009-06-15 03:18:30 +0000

    Added very basic Ajax chat functionality

--- web_games/war/js/lobby.js
+++ web_games/war/js/lobby.js
@@ -1,7 +1,12 @@
+var messageIndex = 0;
+var readyForNewMessages = true;
+
 function initialize() {
 	applyRoundedCorners();
+	getMessageIndex();
 	setupEnterKeyEvent();
 	setInputFocus();
+	setTimeout(getNewMessages, 2000);
 }
 
 function applyRoundedCorners() {
@@ -23,12 +28,52 @@ function setInputFocus() {
 
 function submitChatInput() {
 	var chatInputJquery = $("#chatInput");
+	var messageText = chatInputJquery.attr("value");
+	chatInputJquery.attr("value", "");
+
+	sendMessage(messageText);
+	
+	setInputFocus();
+}
+
+function getMessageIndex() {
+	var json = { "command": "retrieve_message_index" };
+	$.post("lobby.json", json,
+  		function(data){
+			messageIndex = data.message_index;
+  		}, 
+  	"json");
+}
+
+function sendMessage(messageText) {
+	var json = { "command": "add_message", "message": messageText, "message_index": messageIndex };
+	$.post("lobby.json", json,
+  		function(data){
+			updateChatOutput(data.messages);
+			messageIndex = data.message_index;
+  		},
+  	"json");
+}
+
+function getNewMessages() {
+	var json = { "command": "retrieve_messages", "message_index": messageIndex };
+	$.post("lobby.json", json,
+  		function(data){
+			updateChatOutput(data.messages);
+			messageIndex = data.message_index;
+			setTimeout(getNewMessages, 2000);
+  		},
+  	"json");
+}
+
+function updateChatOutput(messages) {
+	var chatInputJquery = $("#chatInput");
 	var chatOutputJquery = $("#chatOutput");
 
-	var outputText = chatInputJquery.attr("value") + "<br/>";
-	chatOutputJquery.append(outputText);
-	chatOutputJquery.attr("scrollTop", chatOutputJquery.attr("scrollHeight"));
+	for (var i = 0; i < messages.length; i++) {
+		var outputText = messages[i] + "<br/>";
+		chatOutputJquery.append(outputText);
+	}
 	
-	chatInputJquery.attr("value", "");
-	setInputFocus();
+	chatOutputJquery.attr("scrollTop", chatOutputJquery.attr("scrollHeight"));
 }
